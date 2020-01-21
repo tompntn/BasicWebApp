f051880049d0fcb7073352924870d8718d81ce44
liampilot <lp616@ic.ac.uk>
2020-01-21 11:17:06 +0000

    fuck you

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -8,7 +8,7 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.toLowerCase().contains("what is your team name")) {
-            return  "bois";
+            return  "bois reborn";
         }
         return "";
     }
